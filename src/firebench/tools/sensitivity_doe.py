c3dc4241892c78322ce398b110bffc3d8ed1fe45
Aurélien Costes <aurelien.costes31@gmail.com>
2024-06-13 11:51:07 -0700

    use SALib instead of scipy for sobol sampling
    update sobol_seq with SALib sobol sequence
    update sensitivity workflow accordingly

--- src/firebench/tools/sensitivity_doe.py
+++ src/firebench/tools/sensitivity_doe.py
@@ -1,11 +1,15 @@
 from typing import Dict, List, Tuple
 
+import numpy as np
 from pint import Quantity, Unit
-from scipy.stats.qmc import Sobol
+from SALib.sample import sobol
 
 
 def sobol_seq(
-    N: float, variables_info: Dict[str, Tuple[Unit, List[float]]], scramble: bool = False, seed: int = None
+    N: int,
+    variables_info: Dict[str, Tuple[Unit, List[float]]],
+    scramble: bool = True,
+    seed: int = 0,
 ) -> Dict[str, Quantity]:
     """
     Generate a Sobol sequence for the given variables with specified units and ranges.
@@ -21,40 +25,33 @@ def sobol_seq(
         A dictionary where each key is the name of a variable, and the value is a tuple containing a `pint.Unit`
         and a list of two floats representing the range of the variable.
     scramble : bool, optional
-        If True, applies scrambling to the Sobol sequence for better uniformity. Defaults to False.
+        If True, applies scrambling to the Sobol sequence for better uniformity. Defaults to True.
     seed : int, optional
-        Seed for the random number generator for reproducibility. Defaults to None.
+        Seed for the random number generator for reproducibility. Defaults to 0.
 
     Returns
     -------
     dict
         A dictionary where each key is a variable name, and the value is a `pint.Quantity` array with the
         specified unit and range.
-
-    Raises
-    ------
-    ValueError
-        If the length of the range list is not equal to 2 or if the upper range value is less than or equal to the lower range value.
+    sobol_problem : dict
+        A dictionary defining the Sobol problem with keys 'num_vars', 'names', and 'bounds'.
+    N_sobol : int
+        The number of points in the generated Sobol sequence.
     """  # pylint: disable=line-too-long
-    nb_variable = len(variables_info.keys())
-    raw_sequence = Sobol(d=nb_variable, scramble=scramble, seed=seed).random(N)
+    sobol_problem = {
+        "num_vars": len(variables_info),
+        "names": [k.value for k in variables_info.keys()],  # Replace with your variable names
+        "bounds": [variables_info[k][1] for k in variables_info.keys()],
+    }
+    sobol_seq = sobol.sample(sobol_problem, N, calc_second_order=True, scramble=scramble, seed=seed)
+    N_sobol = np.size(sobol_seq, 0)
     output_dict = {}
 
     for k, (var_name, var_info) in enumerate(variables_info.items()):
-        var_unit, var_range = var_info
+        output_dict[var_name] = Quantity(sobol_seq[:, k], var_info[0])
 
-        if len(var_range) != 2:
-            raise ValueError(f"Range for variable '{var_name}' must have exactly 2 elements.")
-        if var_range[1] <= var_range[0]:
-            raise ValueError(
-                f"Upper range value must be greater than lower range value for variable '{var_name}'."
-            )
-
-        output_dict[var_name] = Quantity(
-            (var_range[1] - var_range[0]) * raw_sequence[:, k] + var_range[0], var_unit
-        )
-
-    return output_dict
+    return output_dict, sobol_problem, N_sobol
 
 
 def merge_dictionaries(dict1: dict, dict2: dict) -> dict:
--- workflow/rate_of_spread_models_analysis/03_01_sensitivity_env_var.py
+++ workflow/rate_of_spread_models_analysis/03_01_sensitivity_env_var.py
@@ -12,11 +12,12 @@ The analysis considers the impact of environmental variables on fire behavior, s
 This workflow is part of the FireBench project, aimed at systematic benchmarking and inter-comparisons 
 of fire models to enhance their scientific and operational applications.
 """
-import numpy as np
 import firebench.ros_models as rm
 import firebench.tools as fbt
-from firebench import ureg, svn
+import numpy as np
+from firebench import svn, ureg
 from pint import Quantity
+from SALib.analyze import sobol
 
 #######################################################################################
 #                             STEP 1: DESIGN OF EXPERIMENT
@@ -27,37 +28,37 @@ fuel_data = fbt.read_fuel_data_file(
     "Anderson13",
 )
 
-# print(fuel_data)
+# Choose ROS model
+ros_model = rm.Rothermel_SFIRE
 
 # Create wind [m s-1], slope [deg] and moisture [%] data
-nb_sobol_pts = 4  # better if 2^N
-
+## Choose number of point for Sobol sequence
+nb_sobol_pts = 2**14  # better if 2^N
 ## Create the dict of variable name, units and range
 input_vars_info = {
-    svn.WIND: (ureg.meter / ureg.second, [-20, 20]),
-    svn.SLOPE_ANGLE: (ureg.degree, [-45, 45]),
+    svn.WIND: (ureg.meter / ureg.second, [-10, 10]),
+    svn.SLOPE_ANGLE: (ureg.degree, [-30, 30]),
     svn.FUEL_MOISTURE_CONTENT: (ureg.percent, [1, 50]),
 }
 
-sensitivity_vars = fbt.sobol_seq(nb_sobol_pts, input_vars_info)
-
-print(sensitivity_vars)
+## Create the
+input_vars_dict, sobol_problem, nb_pts_total = fbt.sobol_seq(nb_sobol_pts, input_vars_info)
 
 # Concatenate inputs
-input_dict = fbt.merge_dictionaries(fuel_data, sensitivity_vars)
+input_dict = fbt.merge_dictionaries(fuel_data, input_vars_dict)
 
 #######################################################################################
 #                             STEP 2: DATA QUALITY CHECK
 #######################################################################################
 
 # Completness
-fbt.check_input_completeness(input_dict, rm.Rothermel_SFIRE.metadata)
+fbt.check_input_completeness(input_dict, ros_model.metadata)
 
 # unit conversion
-input_converted = fbt.convert_input_data_units(input_dict, rm.Rothermel_SFIRE.metadata)
+input_converted = fbt.convert_input_data_units(input_dict, ros_model.metadata)
 
 # validity range
-fbt.check_validity_range(input_converted, rm.Rothermel_SFIRE.metadata)
+fbt.check_validity_range(input_converted, ros_model.metadata)
 
 # create final input dict
 final_input = input_converted.copy()
@@ -69,18 +70,22 @@ for key, value in final_input.items():
 final_input[svn.FUEL_CLASS] = 1
 # loop over sobol sequence
 
-ros = np.zeros(nb_sobol_pts)
+ros = np.zeros(nb_pts_total)
 ref_dict = final_input.copy()
 
-for k in range(nb_sobol_pts):
-    print(k)
+for k in range(nb_pts_total):
     for key in input_vars_info.keys():
         final_input[key] = ref_dict[key][k]
 
-    ros[k] = rm.Rothermel_SFIRE.compute_ros(final_input)
+    ros[k] = ros_model.compute_ros(final_input)
 
 # Assign unit
 
-ros = Quantity(ros, rm.Rothermel_SFIRE.metadata["output_rate_of_spread"]["units"])
+ros_unit = Quantity(ros, ros_model.metadata["output_rate_of_spread"]["units"])
 
-print(ros)
+# Perform Sobol analysis
+sobol_indices = sobol.analyze(sobol_problem, ros, print_to_console=True)
+
+# Extract first-order and total-order indices
+first_order_indices = sobol_indices["S1"]
+total_order_indices = sobol_indices["ST"]
