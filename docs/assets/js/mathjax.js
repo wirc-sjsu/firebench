bd1a4718dcbd42fc50dab4b57e4ab6926f3405ee
Aurélien Costes <aurelien.costes31@gmail.com>
2024-11-05 11:09:49 -0500

    remove mathjax js

--- docs/assets/js/mathjax.js
+++
@@ -1,8 +0,0 @@
-// assets/js/mathjax.js
-(function () {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.async = true;
-    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
-    document.head.appendChild(script);
-})();
