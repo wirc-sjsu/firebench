4fa6f0a713e7e53fe8ab47cd46b98e5973fbb615
Aurélien Costes <aurelien.costes31@gmail.com>
2024-11-08 09:30:06 -0500

    add Baughman_generalized_wind_reduction_factor_unsheltered function

--- src/firebench/wind_interpolation/__init__.py
+++ src/firebench/wind_interpolation/__init__.py
@@ -1,4 +1,5 @@
 from .wind_reduction_factor import (
     use_wind_reduction_factor,
     Baughman_20ft_wind_reduction_factor_unsheltered,
+    Baughman_generalized_wind_reduction_factor_unsheltered,
 )
