5e32efd2ddba0d2658d8c0fb1222403e59d7f9be
Aurélien Costes <aurelien.costes31@gmail.com>
2025-08-08 16:36:37 -0700

    update regression tests for Anderson 2015

--- tests/regression/test_stats_internal_fuel_models.py
+++ tests/regression/test_stats_internal_fuel_models.py
@@ -3,7 +3,7 @@ from firebench.tools import calculate_sha256
 from firebench.stats import anderson_2015_stats
 
 # Reference hash for figure generated using anderson_2015_stats for different units
-REFERENCE_HASH_1 = "f9e37728e6db114f9663c88470d23a579c4fee6a1bf5aeb1184fbeb570687267"
+REFERENCE_HASH_1 = "c3eb4f765577a3a527829e775c3acc510a57fa3bf06b6a8322ef59e7f5ba56ca"
 REFERENCE_HASH_2 = "d61811fc3865f655143dc8c2d2d667aa1a7875ce9e5ef6e9d37bda083b16a746"
 
 
