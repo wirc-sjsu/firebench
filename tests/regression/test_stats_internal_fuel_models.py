061e13ed96b7ce7e87ccf44ad913566e0dd96f79
Aurélien Costes <aurelien.costes31@gmail.com>
2025-08-09 18:40:33 -0700

    fix hash error in test for anderson_2015_stats

--- tests/regression/test_stats_internal_fuel_models.py
+++ tests/regression/test_stats_internal_fuel_models.py
@@ -1,20 +1,26 @@
 import tempfile
 from firebench.tools import calculate_sha256
 from firebench.stats import anderson_2015_stats
+import hashlib
 
 # Reference hash for figure generated using anderson_2015_stats for different units
-REFERENCE_HASH_1 = "c3eb4f765577a3a527829e775c3acc510a57fa3bf06b6a8322ef59e7f5ba56ca"
-REFERENCE_HASH_2 = "d61811fc3865f655143dc8c2d2d667aa1a7875ce9e5ef6e9d37bda083b16a746"
+REFERENCE_HASH_1 = "1fd65f7add5af642bf4650b9c81b5dfc450b6bf6be985e2fea8aa32b76a5b0cb"
+REFERENCE_HASH_2 = "c1362de1a61d5a3aed2dc2b13c46b7e1117a2ea6e6e27a5ab42a1d93af71857b"
+
+
+def sha256_fig_pixels(fig) -> str:
+    fig.canvas.draw()
+    buf = fig.canvas.buffer_rgba()
+    return hashlib.sha256(memoryview(buf)).hexdigest()
 
 
 def test_anderson_2015_stats_hash_match():
     # unit_wind="m/s", unit_ros="m/s", dpi=150
     with tempfile.NamedTemporaryFile(suffix=".png") as tmp:
-        anderson_2015_stats(output_filename=tmp.name)
-        current_hash = calculate_sha256(tmp.name)
-        assert current_hash == REFERENCE_HASH_1
+        fig = anderson_2015_stats(output_filename=tmp.name, unit_wind="m/s", unit_ros="m/s", dpi=150)
+        assert sha256_fig_pixels(fig) == REFERENCE_HASH_1
+
     # unit_wind="ft/min", unit_ros="km/h", dpi=150
     with tempfile.NamedTemporaryFile(suffix=".png") as tmp:
-        anderson_2015_stats(output_filename=tmp.name)
-        current_hash = calculate_sha256(tmp.name)
-        assert current_hash == REFERENCE_HASH_1
+        fig = anderson_2015_stats(output_filename=tmp.name, unit_wind="ft/min", unit_ros="km/h", dpi=150)
+        assert sha256_fig_pixels(fig) == REFERENCE_HASH_2
